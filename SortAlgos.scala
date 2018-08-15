35fb76a2aa2b70f240f74a5a9049116bce7f96ab
KavabONga <mitya.shabat@gmail.com>
2018-08-15 18:13:09 +0300

    Update SortAlgos.scala

--- SortAlgos.scala
+++ SortAlgos.scala
@@ -9,3 +9,13 @@ def bubbleSort(l : List[Int]) : List[Int] = {
   }
 }
 println(bubbleSort(List( 5, 4, 12, 1, 3)))
+
+def quickSort(l : List[Int]) : List[Int] = {
+  if (l.length <= 1) l
+  else {
+    val a = l.filter(_ <= l.head)
+    val b = l.filterNot(_ <= l.head)
+    quickSort(a.reverse) ::: quickSort(b)
+  }
+}
+println(quickSort(List(5, 3, 12, 1, 4)))
