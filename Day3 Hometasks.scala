031e477112b27a16bbd36fab0cf06ca538ff6d44
KavabONga <mitya.shabat@gmail.com>
2018-08-15 17:40:19 +0300

    Update Day3 Hometasks.scala

--- Day3 Hometasks.scala
+++ Day3 Hometasks.scala
@@ -5,3 +5,20 @@ def countSignChange(l : List[Int]) : Int = {
   })._1
 }
 println(countSignChange(List(1, -1, -1, 1, 1, -1)))
+
+def countCharChange(s : String) : Int = {
+  val vowels = List( 'a', 'e', 'i', 'o', 'u', 'y')
+  val consonants = (('a' to 'z').toSet diff vowels.toSet).toList
+  def varNum(c : Char) = {
+    if (vowels.contains(c)) 0
+    else {
+      if (consonants.contains(c)) 1
+      else -1
+    }
+  }
+  s.tail.foldLeft((0, varNum(s.head.toLower)))((p : (Int, Int), c : Char) => {
+    if (varNum(c.toLower) + p._2 == 1) (p._1 + 1, varNum(c.toLower))
+    else (p._1, varNum(c.toLower))
+  })._1
+}
+println(countCharChange("This is so sad"))
