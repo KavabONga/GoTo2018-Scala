0d63ad18f64c150cfc2f0c3c041026622617b5b1
KavabONga <mitya.shabat@gmail.com>
2018-08-16 18:58:13 +0300

    Update Day4 Tasks.scala

--- Day4 Tasks.scala
+++ Day4 Tasks.scala
@@ -6,47 +6,47 @@ import com.typesafe.scalalogging.LazyLogging
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object UsefulFuture extends LazyLogging {
-  def safeDiv[T](a : Double, b : Double) : Double =
-    Try(Await.result( Future {a / b}, Duration(10, SECONDS))) match {
+  def safeDiv(a : Double, b : Double) : Double = {
+    Try(a / b) match {
       case Success(value) => value
-      case Failure(_) => {
-        logger.error(s"Tried to divide $a by $b")
-        throw new Exception
+      case Failure(exc) => {
+        logger.info("Welp, you've done it")
+        throw exc
       }
     }
-  def bypassOnComplete[A, T](f : Future[T], func: T => A) : Future[A] = {
-    Try(Await.result( f, Duration(10, SECONDS))) match {
-      case Success(_) => f.map(func)
-      case Failure(exception) => Future.failed(exception)
-    }
   }
+  def bypassOnComplete[T](f : Future[T], func: T => T) : Future[T] = f.map(func)
   def ifFailure[T](f : Future[T], funcFail: => Unit, exc : Option[Throwable] = None) : Future[T] = {
-    Try(Await.result( f, Duration(10, SECONDS))) match {
+    f transformWith {
       case Failure(ex) => {
         funcFail
         Future.failed(exc.getOrElse(ex))
       }
-      case Success(value) => f
+      case Success(_) => f
     }
   }
   def completeAndThen[T](firstFuture : Future[T], lastFuture : Future[T]) = {
-    Try(Await.result(firstFuture, Duration(10, SECONDS))) match {
-      case Success(_) => lastFuture
-      case Failure(exc) => firstFuture
+    firstFuture transformWith {
+      case Success(value) =>  {
+        logger.info(s"First result is $value")
+        lastFuture
+      }
+      case Failure(_) => {
+        logger.info("First computation failed")
+        firstFuture
+      }
     }
   }
   def completeAndThenComplete[T](firstFuture : Future[T], lastFuture : Future[T]) = {
-    Try(Await.result(firstFuture, Duration(10, SECONDS))) match {
-      case Success(_) => {
-        Try(Await.result(lastFuture, Duration(10, SECONDS))) match {
-          case Success(value) => lastFuture
-          case Failure(exc) => Future.failed(exc)
+    firstFuture onComplete {
+      case Success(value) =>  {
+        logger.info(s"First result is $value")
+        lastFuture onComplete {
+          case Success(val2) => logger.info(s"Second result is $val2")
+          case Failure(_)    => logger.info("Second computation failed")
         }
       }
-      case Failure(_) => firstFuture
+      case Failure(_) => logger.info("First computation failed")
     }
   }
 }
-
-val f = UsefulFuture.completeAndThenComplete(Future {10}, Future {20})
-println(f)
