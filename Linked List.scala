611f451644f36b6b4d189fca35d6bb45ea77aadf
KavabONga <mitya.shabat@gmail.com>
2018-08-16 15:46:49 +0300

    Update Linked List.scala

--- Linked List.scala
+++ Linked List.scala
@@ -1,6 +1,3 @@
-import scala.annotation.tailrec
-import scala.util.control.Exception
-
 sealed abstract class LinkedList[T] {
   def map[B](func: T => B) : LinkedList[B]
   def ::: (l: LinkedList[T]) : LinkedList[T]
@@ -8,20 +5,20 @@ sealed abstract class LinkedList[T] {
   override def toString: String
 }
 
-case class EmptyBox[T]() extends LinkedList[T] {
-  def map[B](func: T => B) : LinkedList[B] = EmptyBox[B]()
+case class EmptyList[T]() extends LinkedList[T] {
+  def map[B](func: T => B) : LinkedList[B] = EmptyList[B]()
   def ::: (l: LinkedList[T]) : LinkedList[T] = l
   def :+ (v : T) : LinkedList[T] = ListBox[T](v)
   override def toString() = "Empty"
 }
 case class ListBox[T](value : T,
-                      next : LinkedList[T] = EmptyBox[T]()) extends LinkedList[T] {
+                      next : LinkedList[T] = EmptyList[T]()) extends LinkedList[T] {
   private def detachLast(l : LinkedList[T]): LinkedList[T] = {
     l match {
-      case EmptyBox() => throw new Exception
+      case EmptyList() => throw new Exception
       case b : ListBox[T] => {
         b.next match {
-          case EmptyBox() => EmptyBox()
+          case EmptyList() => EmptyList()
           case _ => ListBox(value, detachLast(b.next))
         }
       }
@@ -29,27 +26,28 @@ case class ListBox[T](value : T,
   }
   private def getLast(l : LinkedList[T]): ListBox[T] = {
     l match {
-      case EmptyBox() => throw new Exception
+      case EmptyList() => throw new Exception
       case b : ListBox[T] => {
         b.next match {
-          case EmptyBox() => b
+          case EmptyList() => b
           case _ => getLast(b.next)
         }
       }
     }
   }
   def map[B](func: T => B) : LinkedList[B] =
-     new ListBox[B](func(value), next.map(func))
+    new ListBox[B](func(value), next.map(func))
   def head = value
   def tail = next
   def last = getLast(this).value
   def init = detachLast(this)
 
-  def ::: (l: LinkedList[T]) : LinkedList[T] = ListBox(value, next ::: l)
+  def ::: (l: LinkedList[T]) : LinkedList[T] =
+    (l :+ value) ::: next
   def :+ (v : T) : LinkedList[T] = ListBox(value, next :+ v)
 
   override def toString: String = s"$value -> $next"
 }
-
-val a = EmptyBox() :+ 1 :+ 1 :+ 1
-println(a)
+val a = EmptyList() :+ 1 :+ 2
+val b = EmptyList() :+ 3 :+ 4
+println(a ::: b)
