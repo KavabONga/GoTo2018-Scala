002872e07d38ddce05f42a05dd2dec4add8dd3a8
KavabONga <mitya.shabat@gmail.com>
2018-08-16 16:14:37 +0300

    Update Day4 Tasks.scala

--- Day4 Tasks.scala
+++ Day4 Tasks.scala
@@ -1,5 +1,3 @@
-import UsefulFuture.{logger, safeDiv}
-
 import scala.util._
 import scala.concurrent._
 import scala.concurrent.duration._
@@ -9,26 +7,41 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object UsefulFuture extends LazyLogging {
   def safeDiv[T](a : Double, b : Double) : Double =
-    Try({Await.result( Future {a / b}, Duration(1, SECONDS))}) match {
+    Try(Await.result( Future {a / b}, Duration(10, SECONDS))) match {
       case Success(value) => value
       case Failure(_) => {
         logger.error(s"Tried to divide $a by $b")
         throw new Exception
       }
     }
-  def bypassOnComplete[A, T](f : Future[T], func: T => A) : Future[A] = {
-    f.map(func)
-  }
-  def ifFailure[T](f : Future[T], funcFail: => Unit, exc : Option[Throwable] = None) : Future[T] = {
-    f.onComplete({
+  def bypassOnComplete[A, T](f : Future[T], func: T => A) : Future[A] = f.map(func)
+  def ifFailure[T](f : Future[T], funcFail: => Unit, exc : Option[Throwable] = None) : T = {
+    Try(Await.result( f, Duration(10, SECONDS))) match {
       case Failure(_) => {
-        funcFail()
-        throw exc
+        funcFail
+        throw exc.get
       }
-    })
-    f
+      case Success(value) => value
+    }
+  }
+  def completeAndThen[A, B](firstFuture : Future[A], lastFuture : Future[B]) = {
+    Try(Await.result(firstFuture, Duration(10, SECONDS))) match {
+      case Success(_) => lastFuture
+      case Failure(exc) => throw exc
+    }
+  }
+  def completeAndThenComplete[A, B](firstFuture : Future[A], lastFuture : Future[B]) = {
+    Try(Await.result(firstFuture, Duration(10, SECONDS))) match {
+      case Success(_) => {
+        Try(Await.result(lastFuture, Duration(10, SECONDS))) match {
+          case Success(value) => value
+          case Failure(exc) => throw exc
+        }
+      }
+      case Failure(exc) => throw exc
+    }
   }
 }
 
-val f = UsefulFuture.bypassOnComplete(Future{10}, (x : Int) => x * 2)
-println(Await.result(f, Duration.Inf))
+val f = UsefulFuture.completeAndThenComplete(Future {10}, Future {20})
+println(f)
